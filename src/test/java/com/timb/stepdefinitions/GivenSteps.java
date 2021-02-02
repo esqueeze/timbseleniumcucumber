3e2ddd9a59d017303bd6878590a8e0987ef02707
timb <esqueeze@gmail.com>
2021-02-02 13:24:42 +1100

    closes #3

--- src/test/java/com/timb/stepdefinitions/GivenSteps.java
+++ src/test/java/com/timb/stepdefinitions/GivenSteps.java
@@ -1,12 +1,14 @@
 package com.timb.stepdefinitions;
 
 
+import com.timb.framework.DriverManager;
 import com.timb.pages.HomePage;
 import com.timb.pages.LoginPage;
 import com.timb.stepdefinitions.Objects.Login;
 import com.timb.stepdefinitions.Objects.TextInputField;
-import com.timb.framework.DriverManager;
 import io.cucumber.java.en.Given;
+import org.testng.Reporter;
+import org.testng.xml.XmlTest;
 
 public class GivenSteps extends DriverManager {
 
@@ -18,8 +20,9 @@ public class GivenSteps extends DriverManager {
 
     @Given("I log into LinkedIn")
     public void i_log_into_linked_in() {
-        login.username = new TextInputField("esqueeze@gmail.com");
-        login.password = new TextInputField("nf01th02");
+        XmlTest xmlTest = Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest();
+        login.username = new TextInputField(xmlTest.getParameter("username"));
+        login.password = new TextInputField(xmlTest.getParameter("password"));
         HomePage homePage = new HomePage(driver);
         homePage.login();
         LoginPage loginPage = new LoginPage(driver, login);
